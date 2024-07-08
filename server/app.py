f527e33caeea9be339825c2881eba119f27508e4
Luther Isaboke <lusaboke@gmail.com>
2024-07-08 12:03:18 +0300

    lessom10

--- server/app.py
+++ server/app.py
@@ -19,6 +19,32 @@ db.init_app(app)
 def index():
     body = {'message': 'Welcome to the pet directory!'}
     return make_response(body, 200)
+@app.route('/pets/<int:id>')
+def pet_by_id(id):
+    pet =pet.query.filter(Pet.id==id).first()
+
+    if pet:
+        body={'id':pet.id,
+              'name':pet.name,
+              'species':pet.species
+              }
+        status=200
+    
+    else:
+        body ={'message':f'pet{id} not found'}
+        status=404
+    return make_response(body,status) 
+
+
+@app.route('/species/<string:species>')
+def pet_by_species(species):
+    pets = []  # array to store a dictionary for each pet
+    for pet in Pet.query.filter_by(species=species).all():
+        pets.append(pet.to_dict())
+    body = {'count': len(pets),
+            'pets': pets
+            }
+    return make_response(body, 200)
 
 
 if __name__ == '__main__':
